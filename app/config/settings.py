5be7664b99adcec4a9f4f3bb3f72b46b9458778f
Alexander Aravin <sander-raven@yandex.ru>
2023-04-26 22:01:03 +0300

    Add Materialize CSS

--- app/config/settings.py
+++ app/config/settings.py
@@ -122,6 +122,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

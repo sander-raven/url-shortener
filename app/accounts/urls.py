2f7b958e20254293e09d222d2aada9f26b841408
Alexander Aravin <sander-raven@yandex.ru>
2023-04-27 15:58:21 +0300

    Create a function-based view for the profile page

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('profile/', views.ProfileView.as_view(), name='profile'),
+    # path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/', views.profile_view, name='profile'),
 ]
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 
@@ -17,3 +19,12 @@ class ProfileView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return ShortURL.objects.filter(owner=self.request.user)
+
+
+@login_required
+def profile_view(request):
+    template_name = 'accounts/profile.html'
+    context = {
+        'object_list': ShortURL.objects.filter(owner=request.user)
+    }
+    return render(request, template_name, context)
